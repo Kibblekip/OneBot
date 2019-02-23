8b8e2f9a3afa422badd79fde2848430c083b0e31
Kibble <47759116+Kibblekip@users.noreply.github.com>
2019-02-23 07:23:22 +0000

    (Mostly) completed user.js
    Co-Authored-By: Vincent Del Vecchio <dvecc@users.noreply.github.com>
    Co-Authored-By: Isabella <lewdcario@users.noreply.github.com>
    Co-Authored-By: Spexx <spexxtacular@users.noreply.github.com>

--- commands/user.js
+++ commands/user.js
@@ -1,52 +1,68 @@
 module.exports = {
     name: 'user',
     desc: 'Shows information of a user.',
-    execute(message, args) {
-
-        function UFM(mention) {
-            const matches = mention.match(/^<@!?(\d+)>$/);
-            if (matches) {
-                return message.guild.members.get(matches[1]).user;
-            }
-            else {return null;}
-        }
-
-        function RFM(mention) {
-            const matches = mention.match(/^<@!?(\d+)>$/);
-            if (matches) {
-                return message.guild.roles.get(matches[1]).user;
-            }
-            else {return null;}
-        }
-
-        const user = UFM(args[1]);
-        const role = RFM(args[1]);
-
-        if (user.prefix) {
-            return message.reply('Please mention a user to see their info!')
-        }
-
-        const createTime = user.createdAt;
-        const joinTime = message.member.joinedAt;
+    execute: async function(message, args) {
         
+        let user;
+        let member;
+       
+        if(message.mentions.users.size)
+        {
+            user = message.mentions.users.first();
+            member = message.guild.members.get(user.id);
+        }
+       
+        else if(args[0])
+        {
+            user = await message.client.fetchUser(args[0]);
+            if (!user) {
+                console.log(user);
+                console.log(message.client.users.size, message.client.users.has(args[0]), args)
+                return message.channel.send('[This user ID is invalid!]');
+            }
+            member = message.guild.members.get(user.id);
+        }
+
+        else
+        {
+            user = message.author;
+            member = message.member;
+        }
+       
+        const createTime = user.createdAt;
+       
         let Csec = createTime.getSeconds().toString().length === 1 ? '0' + createTime.getSeconds() : createTime.getSeconds();
         let Cmin = createTime.getMinutes().toString().length === 1 ? '0' + createTime.getMinutes() : createTime.getMinutes();
         let Chour = createTime.getHours().toString().length === 1 ? '0' + createTime.getHours() : createTime.getHours();
-
-        let Jsec = joinTime.getSeconds().toString().length === 1 ? '0' + joinTime.getSeconds() : joinTime.getSeconds();
-        let Jmin = joinTime.getMinutes().toString().length === 1 ? '0' + joinTime.getMinutes() : joinTime.getMinutes();
-        let Jhour = joinTime.getHours().toString().length === 1 ? '0' + joinTime.getHours() : joinTime.getHours();
-
+       
         const embed = new Discord.RichEmbed()
-
-        if (guild.member(UFM)) {
-            embed
-            .setTitle(`${UFM.username} [${UFM.user.id}]`)
+        
+            .setTitle(`${user.username} [${user.id}]`)
             .setColor(0xFFDC56)
-            .setThumbnail(UFM.avatarURL)
-            .addField('[Account created at...]', createTime.toDateString() + ', ' + Chour + ':' + Cmin + ':' + Csec + ', GMT')
+            .setThumbnail(user.avatarURL)
+            .addField('[Account created at...]', createTime.toDateString() + ', ' + Chour + ':' + Cmin + ':' + Csec + ', GMT');
+        
+        if(member)
+        {
+            const joinTime = member.joinedAt;
+            let Jsec = joinTime.getSeconds().toString().length === 1 ? '0' + joinTime.getSeconds() : joinTime.getSeconds();
+            let Jmin = joinTime.getMinutes().toString().length === 1 ? '0' + joinTime.getMinutes() : joinTime.getMinutes();
+            let Jhour = joinTime.getHours().toString().length === 1 ? '0' + joinTime.getHours() : joinTime.getHours();
+            
+             embed
             .addField('[Joined server at...]', joinTime.toDateString() + ', ' + Jhour + ':' + Jmin + ':' + Jsec + ', GMT')
-            .addField(`[${UFM.username} has the following roles...]`, role.roles.map(r => r.name).join)
-            }
-    }
+            .addField(`[${user.username} has the following roles...]`, member.roles.map(r => r.name).join(', '));
+        
+        }
+        
+        if(user.id === '513805373803593731')
+        {
+            let kibID = client.users.get('179615735365107712');
+            embed
+            .addField('[What am I?]', 'I am a Discord bot, programmed in JavaScript and made by Kibblekip!')
+        }
+    
+        message.channel.send(embed);
+        console.log(`${message.author.tag} (${message.author.id}) just checked for ${user.tag} (${user.id})'s info!`);
+    }   
 }
